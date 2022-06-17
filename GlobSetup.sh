a2c7c53c99cab289fd80f768825faa7e429f74b8
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-06-17 18:30:38 +0100

    Update GlobSetup.sh

--- GlobSetup.sh
+++ GlobSetup.sh
@@ -3,7 +3,7 @@
 clear
 
 sudo apt update
-sudo apt-fast install xfce4-appmenu-plugin vala-panel-appmenu appmenu* -y --no-install-recommends --no-install-suggests 
+sudo apt install xfce4-appmenu-plugin vala-panel-appmenu appmenu* -y --no-install-recommends --no-install-suggests 
 xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
 xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
 sudo cp 52appmenu-gtk-module_add-to-gtk-modules /etc/X11/Xsession.d
